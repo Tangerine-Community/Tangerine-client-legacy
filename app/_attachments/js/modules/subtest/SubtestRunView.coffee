2e208ac23a867c022e11eb5381d3e6a697041e55
fet mar <fetmar@gmail.com>
2012-05-15 10:21:52 -0400

    Subtest run view owns prototype views

---
+++ app/_attachments/js/modules/subtest/SubtestRunView.coffee
@@ -0,0 +1,77 @@
+class SubtestRunView extends Backbone.View
+  
+  events:
+    "click .next"         : "next"
+    "click .subtest_help" : "toggleHelp"
+
+  toggleHelp: -> @$el.find(".enumerator_help").fadeToggle(250)
+
+  initialize: (options) ->
+    @protoViews  = Tangerine.config.prototypeViews
+    @model       = options.model
+    @parent      = options.parent
+
+
+  render: ->
+    # in case we hit a back button accidentally
+    # window.onbeforeunload = => "Assessment is still running"
+
+    enumeratorHelp = if (@model.get("enumeratorHelp") || "") != "" then "<button class='subtest_help command'>help</button><div class='enumerator_help'>#{@model.get 'enumeratorHelp'}</div>" else ""
+    studentDialog  = if (@model.get("studentDialog") || "") != "" then "<div class='student_dialog command'>#{@model.get 'studentDialog'}</div>" else ""
+
+    @$el.html "
+      <h2>#{@model.get 'name'}</h2>
+
+      #{enumeratorHelp}
+      #{studentDialog}
+      
+    "
+
+    # Use prototype specific views here
+    @prototypeView = new window[@protoViews[@model.get 'prototype']]
+      model: @model
+      parent: @
+    @prototypeView.render()
+    @$el.append @prototypeView.el
+
+    @$el.append "<button class='next navigation'>Next</button>"
+
+    @trigger "rendered"
+
+  getGridScore: ->
+    link = @model.get("gridLinkId") || ""
+    if link == "" then throw "subtest grid link unspecified"; return
+    grid = @parent.model.subtests.get @model.get("gridLinkId")
+    gridScore = @parent.result.getGridScore grid.id
+    gridScore 
+
+  onClose: ->
+    @prototypeView?.close?()
+
+  isValid: ->
+    if not @prototypeView.isValid?()
+      return false
+    true
+
+  showErrors: ->
+    @prototypeView.showErrors()
+
+  getSum: ->
+    if @prototypeView.getSum?
+      return @prototypeView.getSum()
+    else
+      # maybe a better fallback
+      return {correct:0,incorrect:0,missing:0,total:0}
+
+  abort: ->
+    @parent.abort()
+
+  getResult: ->
+    if @prototypeView.getResult?
+      return @prototypeView.getResult()
+    else
+      return @$el.find("form").serializeSubtest()
+
+  next: ->
+    @parent.next()
+  
---
+++ app/_attachments/js/modules/subtest/SubtestRunView.js
@@ -0,0 +1,103 @@
+var SubtestRunView,
+  __hasProp = Object.prototype.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+SubtestRunView = (function(_super) {
+
+  __extends(SubtestRunView, _super);
+
+  function SubtestRunView() {
+    SubtestRunView.__super__.constructor.apply(this, arguments);
+  }
+
+  SubtestRunView.prototype.events = {
+    "click .next": "next",
+    "click .subtest_help": "toggleHelp"
+  };
+
+  SubtestRunView.prototype.toggleHelp = function() {
+    return this.$el.find(".enumerator_help").fadeToggle(250);
+  };
+
+  SubtestRunView.prototype.initialize = function(options) {
+    this.protoViews = Tangerine.config.prototypeViews;
+    this.model = options.model;
+    return this.parent = options.parent;
+  };
+
+  SubtestRunView.prototype.render = function() {
+    var enumeratorHelp, studentDialog;
+    enumeratorHelp = (this.model.get("enumeratorHelp") || "") !== "" ? "<button class='subtest_help command'>help</button><div class='enumerator_help'>" + (this.model.get('enumeratorHelp')) + "</div>" : "";
+    studentDialog = (this.model.get("studentDialog") || "") !== "" ? "<div class='student_dialog command'>" + (this.model.get('studentDialog')) + "</div>" : "";
+    this.$el.html("      <h2>" + (this.model.get('name')) + "</h2>      " + enumeratorHelp + "      " + studentDialog + "          ");
+    this.prototypeView = new window[this.protoViews[this.model.get('prototype')]]({
+      model: this.model,
+      parent: this
+    });
+    this.prototypeView.render();
+    this.$el.append(this.prototypeView.el);
+    this.$el.append("<button class='next navigation'>Next</button>");
+    return this.trigger("rendered");
+  };
+
+  SubtestRunView.prototype.getGridScore = function() {
+    var grid, gridScore, link;
+    link = this.model.get("gridLinkId") || "";
+    if (link === "") {
+      throw "subtest grid link unspecified";
+      return;
+    }
+    grid = this.parent.model.subtests.get(this.model.get("gridLinkId"));
+    gridScore = this.parent.result.getGridScore(grid.id);
+    return gridScore;
+  };
+
+  SubtestRunView.prototype.onClose = function() {
+    var _ref;
+    return (_ref = this.prototypeView) != null ? typeof _ref.close === "function" ? _ref.close() : void 0 : void 0;
+  };
+
+  SubtestRunView.prototype.isValid = function() {
+    var _base;
+    if (!(typeof (_base = this.prototypeView).isValid === "function" ? _base.isValid() : void 0)) {
+      return false;
+    }
+    return true;
+  };
+
+  SubtestRunView.prototype.showErrors = function() {
+    return this.prototypeView.showErrors();
+  };
+
+  SubtestRunView.prototype.getSum = function() {
+    if (this.prototypeView.getSum != null) {
+      return this.prototypeView.getSum();
+    } else {
+      return {
+        correct: 0,
+        incorrect: 0,
+        missing: 0,
+        total: 0
+      };
+    }
+  };
+
+  SubtestRunView.prototype.abort = function() {
+    return this.parent.abort();
+  };
+
+  SubtestRunView.prototype.getResult = function() {
+    if (this.prototypeView.getResult != null) {
+      return this.prototypeView.getResult();
+    } else {
+      return this.$el.find("form").serializeSubtest();
+    }
+  };
+
+  SubtestRunView.prototype.next = function() {
+    return this.parent.next();
+  };
+
+  return SubtestRunView;
+
+})(Backbone.View);
