efc78337b41defe6ae9caa3798f43849319112ec
fet mar <fetmar@gmail.com>
2012-05-15 10:16:10 -0400

    editing assessments

---
+++ app/_attachments/js/modules/assessment/AssessmentEditView.coffee
@@ -0,0 +1,144 @@
+class AssessmentEditView extends Backbone.View
+
+  className : 'assessmentEditView'
+
+  events :
+    'click #archive_buttons input' : 'save'
+    'click .back'               : 'back'
+    'click .new_subtest_button' : 'showNewSubtestForm'
+    'click .new_subtest_cancel' : 'hideNewSubtestForm'
+    'click .new_subtest_save'   : 'saveNewSubtest'
+    'change #basic input' : 'showSave'
+    'click .assessment_save' : 'save'
+
+  save: =>
+    @updateModel()
+    if @model.save() 
+      Utils.midAlert "Assessment saved" 
+      Tangerine.router.navigate "edit/"+@model.get("name"), true
+      @render()
+
+  showSave: -> @$el.find('.assessment_save').fadeIn(250)
+
+  back: ->
+    Tangerine.router.navigate "", true
+
+  updateModel: =>
+    @model.set
+      archived : @$el.find("#archive_buttons input:checked").val()
+      name     : @$el.find("#assessment_name").val()
+      dKey     : @$el.find("#assessment_d_key").val()
+      assessmentId : @model.id
+
+
+  showNewSubtestForm: -> @$el.find(".new_subtest_form").fadeIn(250)
+  hideNewSubtestForm: -> @$el.find(".new_subtest_form").fadeOut(250)
+  saveNewSubtest: ->
+    # general template
+    newAttributes = Tangerine.config.subtestTemplate
+    
+    # prototype template
+    prototypeTemplate = Tangerine.config.prototypeTemplates[@$el.find("#subtest_type_select").val()]
+    
+    # bit more specific template
+    useType = @$el.find("#subtest_type_select :selected").attr 'data-template'
+    useTypeTemplate = Tangerine.config.subtestTemplates[@$el.find("#subtest_type_select").val()][useType]
+
+    newAttributes = $.extend newAttributes, prototypeTemplate
+    newAttributes = $.extend newAttributes, useTypeTemplate
+    newAttributes = $.extend newAttributes,
+      name         : @$el.find("#new_subtest_name").val()
+      assessmentId : @model.id
+      order        : @model.subtests.length
+    newSubtest = @model.subtests.create newAttributes
+  
+  deleteSubtest: (model) =>
+    @model.subtests.remove model
+    model.destroy()
+  
+  initialize: (options) ->
+    @views = []
+    @model = options.model
+    @model.subtests.on "change remove", @render
+
+  render: =>
+    console.log "model"
+    console.log @model
+    arch = @model.get('archived')
+    archiveChecked    = if (arch == true or arch == 'true') then "checked" else ""
+    notArchiveChecked = if archiveChecked then "" else "checked"
+    @$el.html "
+      <button class='back navigation'>Back</button>
+        <h1>Assessment Builder</h1>
+      <div id='basic'>
+        <label for='assessment_name'>Name</label>
+        <input id='assessment_name' value='#{@model.get("name")}'>
+        <button class='assessment_save confirmation'>Save</button>
+
+        <label for='assessment_d_key'>Download Key</label>
+        <div class='info_box'>#{@model.id.substr(-5,5)}</div>
+      </div>
+
+      <div id='archive_buttons'>
+        <input type='radio' id='archive_false' name='archive' value='false' #{notArchiveChecked}><label for='archive_false'>Active</label>
+        <input type='radio' id='archive_true'  name='archive' value='true'  #{archiveChecked}><label for='archive_true'>Archived</label>
+      </div>
+      <h2>Subtests</h2>
+      <button class='new_subtest_button command'>New</button>
+      <div class='new_subtest_form confirmation'>
+        <div class='label_value'>
+          <label for='new_subtest_type'>Type</label>
+          <div id='subtest_type'></div>
+        </div>
+        <div class='label_value'>
+          <label for='new_subtest_name'>Name</label>
+          <input type='text' id='new_subtest_name'>
+        </div>
+        <button class='new_subtest_save command'>Save</button><button class='new_subtest_cancel command'>cancel</button>
+      </div>
+    "
+
+    # insert a list of templates
+    subtestTypeSelect = "<select id='subtest_type_select'>
+      <option value='' disabled='disabled' selected='selected'>Please select a subtest type</option>"
+    for key, value of Tangerine.config.subtestTemplates
+      subtestTypeSelect += "<optgroup label='#{key}'>"
+      for subKey, subValue of value
+        subtestTypeSelect += "<option value='#{key}' data-template='#{subKey}'>#{subKey}</option>"
+      subtestTypeSelect += "</optgroup>"
+    subtestTypeSelect += "</select>"
+    @$el.find("#subtest_type").html subtestTypeSelect
+
+    # buttonset archive buttons
+    @$el.find("#archive_buttons").buttonset()
+
+    
+    # render new subtest views
+    unorderedList = $('<ul>').attr('id', 'subtest_list')
+    @closeViews()
+    @views = []
+    @model.subtests.sort()
+    @model.subtests.each (subtest) =>
+      oneView = new SubtestListElementView
+        model : subtest
+      @views.push oneView
+      oneView.render()
+      oneView.on "subtest:delete", @deleteSubtest
+      unorderedList.append oneView.el
+    @$el.append unorderedList
+    
+    # make it sortable
+    @$el.find("#subtest_list").sortable
+      handle : '.sortable_handle'
+      update : (event, ui) =>
+        for id, i in ($(li).attr("data-id") for li in @$el.find("#subtest_list li"))
+          @model.subtests.get(id).set({"order":i},{silent:true}).save(null,{silent:true})
+
+    @trigger "rendered"
+
+  onClose: ->
+    @closeViews()
+    
+  closeViews: ->
+    for view in @views
+      view.close()
---
+++ app/_attachments/js/modules/assessment/AssessmentEditView.js
@@ -0,0 +1,177 @@
+var AssessmentEditView,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  __hasProp = Object.prototype.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+AssessmentEditView = (function(_super) {
+
+  __extends(AssessmentEditView, _super);
+
+  function AssessmentEditView() {
+    this.render = __bind(this.render, this);
+    this.deleteSubtest = __bind(this.deleteSubtest, this);
+    this.updateModel = __bind(this.updateModel, this);
+    this.save = __bind(this.save, this);
+    AssessmentEditView.__super__.constructor.apply(this, arguments);
+  }
+
+  AssessmentEditView.prototype.className = 'assessmentEditView';
+
+  AssessmentEditView.prototype.events = {
+    'click #archive_buttons input': 'save',
+    'click .back': 'back',
+    'click .new_subtest_button': 'showNewSubtestForm',
+    'click .new_subtest_cancel': 'hideNewSubtestForm',
+    'click .new_subtest_save': 'saveNewSubtest',
+    'change #basic input': 'showSave',
+    'click .assessment_save': 'save'
+  };
+
+  AssessmentEditView.prototype.save = function() {
+    this.updateModel();
+    if (this.model.save()) {
+      Utils.midAlert("Assessment saved");
+      Tangerine.router.navigate("edit/" + this.model.get("name"), true);
+      return this.render();
+    }
+  };
+
+  AssessmentEditView.prototype.showSave = function() {
+    return this.$el.find('.assessment_save').fadeIn(250);
+  };
+
+  AssessmentEditView.prototype.back = function() {
+    return Tangerine.router.navigate("", true);
+  };
+
+  AssessmentEditView.prototype.updateModel = function() {
+    return this.model.set({
+      archived: this.$el.find("#archive_buttons input:checked").val(),
+      name: this.$el.find("#assessment_name").val(),
+      dKey: this.$el.find("#assessment_d_key").val(),
+      assessmentId: this.model.id
+    });
+  };
+
+  AssessmentEditView.prototype.showNewSubtestForm = function() {
+    return this.$el.find(".new_subtest_form").fadeIn(250);
+  };
+
+  AssessmentEditView.prototype.hideNewSubtestForm = function() {
+    return this.$el.find(".new_subtest_form").fadeOut(250);
+  };
+
+  AssessmentEditView.prototype.saveNewSubtest = function() {
+    var newAttributes, newSubtest, prototypeTemplate, useType, useTypeTemplate;
+    newAttributes = Tangerine.config.subtestTemplate;
+    prototypeTemplate = Tangerine.config.prototypeTemplates[this.$el.find("#subtest_type_select").val()];
+    useType = this.$el.find("#subtest_type_select :selected").attr('data-template');
+    useTypeTemplate = Tangerine.config.subtestTemplates[this.$el.find("#subtest_type_select").val()][useType];
+    newAttributes = $.extend(newAttributes, prototypeTemplate);
+    newAttributes = $.extend(newAttributes, useTypeTemplate);
+    newAttributes = $.extend(newAttributes, {
+      name: this.$el.find("#new_subtest_name").val(),
+      assessmentId: this.model.id,
+      order: this.model.subtests.length
+    });
+    return newSubtest = this.model.subtests.create(newAttributes);
+  };
+
+  AssessmentEditView.prototype.deleteSubtest = function(model) {
+    this.model.subtests.remove(model);
+    return model.destroy();
+  };
+
+  AssessmentEditView.prototype.initialize = function(options) {
+    this.views = [];
+    this.model = options.model;
+    return this.model.subtests.on("change remove", this.render);
+  };
+
+  AssessmentEditView.prototype.render = function() {
+    var arch, archiveChecked, key, notArchiveChecked, subKey, subValue, subtestTypeSelect, unorderedList, value, _ref,
+      _this = this;
+    console.log("model");
+    console.log(this.model);
+    arch = this.model.get('archived');
+    archiveChecked = arch === true || arch === 'true' ? "checked" : "";
+    notArchiveChecked = archiveChecked ? "" : "checked";
+    this.$el.html("      <button class='back navigation'>Back</button>        <h1>Assessment Builder</h1>      <div id='basic'>        <label for='assessment_name'>Name</label>        <input id='assessment_name' value='" + (this.model.get("name")) + "'>        <button class='assessment_save confirmation'>Save</button>        <label for='assessment_d_key'>Download Key</label>        <div class='info_box'>" + (this.model.id.substr(-5, 5)) + "</div>      </div>      <div id='archive_buttons'>        <input type='radio' id='archive_false' name='archive' value='false' " + notArchiveChecked + "><label for='archive_false'>Active</label>        <input type='radio' id='archive_true'  name='archive' value='true'  " + archiveChecked + "><label for='archive_true'>Archived</label>      </div>      <h2>Subtests</h2>      <button class='new_subtest_button command'>New</button>      <div class='new_subtest_form confirmation'>        <div class='label_value'>          <label for='new_subtest_type'>Type</label>          <div id='subtest_type'></div>        </div>        <div class='label_value'>          <label for='new_subtest_name'>Name</label>          <input type='text' id='new_subtest_name'>        </div>        <button class='new_subtest_save command'>Save</button><button class='new_subtest_cancel command'>cancel</button>      </div>    ");
+    subtestTypeSelect = "<select id='subtest_type_select'>      <option value='' disabled='disabled' selected='selected'>Please select a subtest type</option>";
+    _ref = Tangerine.config.subtestTemplates;
+    for (key in _ref) {
+      value = _ref[key];
+      subtestTypeSelect += "<optgroup label='" + key + "'>";
+      for (subKey in value) {
+        subValue = value[subKey];
+        subtestTypeSelect += "<option value='" + key + "' data-template='" + subKey + "'>" + subKey + "</option>";
+      }
+      subtestTypeSelect += "</optgroup>";
+    }
+    subtestTypeSelect += "</select>";
+    this.$el.find("#subtest_type").html(subtestTypeSelect);
+    this.$el.find("#archive_buttons").buttonset();
+    unorderedList = $('<ul>').attr('id', 'subtest_list');
+    this.closeViews();
+    this.views = [];
+    this.model.subtests.sort();
+    this.model.subtests.each(function(subtest) {
+      var oneView;
+      oneView = new SubtestListElementView({
+        model: subtest
+      });
+      _this.views.push(oneView);
+      oneView.render();
+      oneView.on("subtest:delete", _this.deleteSubtest);
+      return unorderedList.append(oneView.el);
+    });
+    this.$el.append(unorderedList);
+    this.$el.find("#subtest_list").sortable({
+      handle: '.sortable_handle',
+      update: function(event, ui) {
+        var i, id, li, _len, _ref2, _results;
+        _ref2 = (function() {
+          var _i, _len, _ref2, _results2;
+          _ref2 = this.$el.find("#subtest_list li");
+          _results2 = [];
+          for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+            li = _ref2[_i];
+            _results2.push($(li).attr("data-id"));
+          }
+          return _results2;
+        }).call(_this);
+        _results = [];
+        for (i = 0, _len = _ref2.length; i < _len; i++) {
+          id = _ref2[i];
+          _results.push(_this.model.subtests.get(id).set({
+            "order": i
+          }, {
+            silent: true
+          }).save(null, {
+            silent: true
+          }));
+        }
+        return _results;
+      }
+    });
+    return this.trigger("rendered");
+  };
+
+  AssessmentEditView.prototype.onClose = function() {
+    return this.closeViews();
+  };
+
+  AssessmentEditView.prototype.closeViews = function() {
+    var view, _i, _len, _ref, _results;
+    _ref = this.views;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      view = _ref[_i];
+      _results.push(view.close());
+    }
+    return _results;
+  };
+
+  return AssessmentEditView;
+
+})(Backbone.View);
