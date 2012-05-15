2336d43c5b834baf5fc8739c5a976a1c2676dd22
fet mar <fetmar@gmail.com>
2012-05-15 10:16:43 -0400

    this view runs an assessment

---
+++ app/_attachments/js/modules/assessment/AssessmentRunView.coffee
@@ -0,0 +1,67 @@
+class AssessmentRunView extends Backbone.View
+  
+  initialize: (options) ->
+    @abortAssessment = false
+    @index = 0
+    @model = options.model
+    @result = new Result
+      assessmentId : @model.id
+      assessmentName : @model.get "name"
+    @subtestViews = []
+    @model.subtests.each (model) =>
+      @subtestViews.push new SubtestRunView 
+        model  : model
+        parent : @
+    resultView = new ResultView
+        model          : @result
+        assessment     : @model
+        assessmentView : @
+    resultView.on "assessment:restart", @restart
+    @subtestViews.push resultView
+        
+  render: ->
+    currentView = @subtestViews[@index]
+    
+    if @model.subtests.length == 0
+      @$el.append "<h1>Oops...</h1><p>This assessment is blank. Perhaps you meant to add some subtests.</p>"
+    else
+      @$el.html "
+        <h1>#{@model.get 'name'}</h1>
+        <div id='progress'></div>
+      "
+      @$el.find('#progress').progressbar value : ((@index+1)/(@model.subtests.length+1)*100)
+      currentView.render()
+      @$el.append currentView.el
+
+    @trigger "rendered"
+
+  onClose: ->
+    for view in @subtestViews
+      view.close()
+    @result.clear()
+    $("#current_student_id").fadeOut(250, -> $(@).html(""))
+    $("#current_student").fadeOut(250)
+
+  abort: ->
+    @abortAssessment = true
+    @next()
+
+  restart: =>
+    alert 'assessment restart not implemented'
+
+  next:->
+    currentView = @subtestViews[@index]
+    console.log currentView
+    if currentView.isValid()
+      @result.add
+        name : currentView.model.get "name"
+        data : currentView.getResult()
+        subtestId : currentView.model.id
+        sum : currentView.getSum()
+      currentView.close()
+      @index++ unless @abortAssessment == true
+      @index = @subtestViews.length-1 if @abortAssessment == true
+      @render()
+      window.scrollTo 0, 0
+    else
+      currentView.showErrors()
---
+++ app/_attachments/js/modules/assessment/AssessmentRunView.js
@@ -0,0 +1,103 @@
+var AssessmentRunView,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  __hasProp = Object.prototype.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+AssessmentRunView = (function(_super) {
+
+  __extends(AssessmentRunView, _super);
+
+  function AssessmentRunView() {
+    this.restart = __bind(this.restart, this);
+    AssessmentRunView.__super__.constructor.apply(this, arguments);
+  }
+
+  AssessmentRunView.prototype.initialize = function(options) {
+    var resultView,
+      _this = this;
+    this.abortAssessment = false;
+    this.index = 0;
+    this.model = options.model;
+    this.result = new Result({
+      assessmentId: this.model.id,
+      assessmentName: this.model.get("name")
+    });
+    this.subtestViews = [];
+    this.model.subtests.each(function(model) {
+      return _this.subtestViews.push(new SubtestRunView({
+        model: model,
+        parent: _this
+      }));
+    });
+    resultView = new ResultView({
+      model: this.result,
+      assessment: this.model,
+      assessmentView: this
+    });
+    resultView.on("assessment:restart", this.restart);
+    return this.subtestViews.push(resultView);
+  };
+
+  AssessmentRunView.prototype.render = function() {
+    var currentView;
+    currentView = this.subtestViews[this.index];
+    if (this.model.subtests.length === 0) {
+      this.$el.append("<h1>Oops...</h1><p>This assessment is blank. Perhaps you meant to add some subtests.</p>");
+    } else {
+      this.$el.html("        <h1>" + (this.model.get('name')) + "</h1>        <div id='progress'></div>      ");
+      this.$el.find('#progress').progressbar({
+        value: (this.index + 1) / (this.model.subtests.length + 1) * 100
+      });
+      currentView.render();
+      this.$el.append(currentView.el);
+    }
+    return this.trigger("rendered");
+  };
+
+  AssessmentRunView.prototype.onClose = function() {
+    var view, _i, _len, _ref;
+    _ref = this.subtestViews;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      view = _ref[_i];
+      view.close();
+    }
+    this.result.clear();
+    $("#current_student_id").fadeOut(250, function() {
+      return $(this).html("");
+    });
+    return $("#current_student").fadeOut(250);
+  };
+
+  AssessmentRunView.prototype.abort = function() {
+    this.abortAssessment = true;
+    return this.next();
+  };
+
+  AssessmentRunView.prototype.restart = function() {
+    return alert('assessment restart not implemented');
+  };
+
+  AssessmentRunView.prototype.next = function() {
+    var currentView;
+    currentView = this.subtestViews[this.index];
+    console.log(currentView);
+    if (currentView.isValid()) {
+      this.result.add({
+        name: currentView.model.get("name"),
+        data: currentView.getResult(),
+        subtestId: currentView.model.id,
+        sum: currentView.getSum()
+      });
+      currentView.close();
+      if (this.abortAssessment !== true) this.index++;
+      if (this.abortAssessment === true) this.index = this.subtestViews.length - 1;
+      this.render();
+      return window.scrollTo(0, 0);
+    } else {
+      return currentView.showErrors();
+    }
+  };
+
+  return AssessmentRunView;
+
+})(Backbone.View);
