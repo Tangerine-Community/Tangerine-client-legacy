0addd568b710e4fed16f1c757936c4df85c16eeb
fet mar <fetmar@gmail.com>
2012-05-15 09:37:21 -0400

    finally understand defaults, url, and working with fetch
    However, when editing a subtest the subtest only knows what the ID of
    the assessment it belongs to. I think maybe a simple fetch to get the
    name and get rid of superFetch, which uses _id, instead of name. seems
    costly, wish we could throw around objects without risking memory leaks

--- app/_attachments/js/modules/assessment/Assessment.coffee
+++ app/_attachments/js/modules/assessment/Assessment.coffee
@@ -1,22 +1,49 @@
 class Assessment extends Backbone.Model
 
-  url: '/assessment'
+  url: 'assessment'
 
   defaults:
-    name     : "Untitled"
-    group    : "default"
-    author   : "admin"
-    subtests : []
+    name       : "Untitled"
+    group      : "default"
+    
+  initialize: (options={}) ->
+    # this collection doesn't get saved
+    # changes update the subtest view, it keeps order
+    @subtests = new Subtests
 
-  initialize: (options) ->
-    @set
-      name     : options?.name     ? @defaults.name
-      group    : options?.group    ? @defaults.group
-      author   : options?.author   ? @defaults.author
-      subtests : options?.subtests ? @defaults.subtests
-
+  # Hijacked success() for later
+  # fetchs all subtests for the assessment
   fetch: (options) ->
-    super(options)
-    subtests
+    options.name = Utils.cleanURL options.name if options.name?
+    allAssessments = new Assessments  
+    allAssessments.fetch
+      success: (collection) =>
+        results = collection.where
+          "name"  : options.name
 
-  
+        for assessment in results
+          if Tangerine.context.server
+            if (~Tangerine.user.groups.indexOf(assessment.get("group")) )
+              @constructor assessment.attributes
+          else
+            @constructor assessment.attributes
+            
+        allSubtests = new Subtests
+        allSubtests.fetch
+          success: (collection) =>
+            @subtests = new Subtests(collection.where { 'assessmentId' : @id } )
+            @subtests.maintainOrder()
+            options.success @
+
+  # this is for the subtest edit back button, probably a better way
+  superFetch: (options) =>
+    
+    Assessment.__super__.fetch.call @,
+      success: (model) =>
+      allSubtests = new Subtests
+      allSubtests.fetch
+        success: (collection) =>
+          @subtests = new Subtests(collection.where { 'assessmentId' : @id } )
+          @subtests.maintainOrder()
+          options.success @
+
--- app/_attachments/js/modules/assessment/Assessment.js
+++ app/_attachments/js/modules/assessment/Assessment.js
@@ -1,4 +1,5 @@
 var Assessment,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
   __hasProp = Object.prototype.hasOwnProperty,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
@@ -7,31 +8,71 @@ Assessment = (function(_super) {
   __extends(Assessment, _super);
 
   function Assessment() {
+    this.superFetch = __bind(this.superFetch, this);
     Assessment.__super__.constructor.apply(this, arguments);
   }
 
-  Assessment.prototype.url = '/assessment';
+  Assessment.prototype.url = 'assessment';
 
   Assessment.prototype.defaults = {
     name: "Untitled",
-    group: "default",
-    author: "admin",
-    subtests: []
+    group: "default"
   };
 
   Assessment.prototype.initialize = function(options) {
-    var _ref, _ref2, _ref3, _ref4;
-    return this.set({
-      name: (_ref = options != null ? options.name : void 0) != null ? _ref : this.defaults.name,
-      group: (_ref2 = options != null ? options.group : void 0) != null ? _ref2 : this.defaults.group,
-      author: (_ref3 = options != null ? options.author : void 0) != null ? _ref3 : this.defaults.author,
-      subtests: (_ref4 = options != null ? options.subtests : void 0) != null ? _ref4 : this.defaults.subtests
-    });
+    if (options == null) options = {};
+    return this.subtests = new Subtests;
   };
 
   Assessment.prototype.fetch = function(options) {
-    Assessment.__super__.fetch.call(this, options);
-    return subtests;
+    var allAssessments,
+      _this = this;
+    if (options.name != null) options.name = Utils.cleanURL(options.name);
+    allAssessments = new Assessments;
+    return allAssessments.fetch({
+      success: function(collection) {
+        var allSubtests, assessment, results, _i, _len;
+        results = collection.where({
+          "name": options.name
+        });
+        for (_i = 0, _len = results.length; _i < _len; _i++) {
+          assessment = results[_i];
+          if (Tangerine.context.server) {
+            if (~Tangerine.user.groups.indexOf(assessment.get("group"))) {
+              _this.constructor(assessment.attributes);
+            }
+          } else {
+            _this.constructor(assessment.attributes);
+          }
+        }
+        allSubtests = new Subtests;
+        return allSubtests.fetch({
+          success: function(collection) {
+            _this.subtests = new Subtests(collection.where({
+              'assessmentId': _this.id
+            }));
+            _this.subtests.maintainOrder();
+            return options.success(_this);
+          }
+        });
+      }
+    });
+  };
+
+  Assessment.prototype.superFetch = function(options) {
+    var allSubtests,
+      _this = this;
+    return Assessment.__super__.fetch.call(this, {
+      success: function(model) {}
+    }, allSubtests = new Subtests, allSubtests.fetch({
+      success: function(collection) {
+        _this.subtests = new Subtests(collection.where({
+          'assessmentId': _this.id
+        }));
+        _this.subtests.maintainOrder();
+        return options.success(_this);
+      }
+    }));
   };
 
   return Assessment;
