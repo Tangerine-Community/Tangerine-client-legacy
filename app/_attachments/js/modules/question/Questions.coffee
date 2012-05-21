93224354b226d577b22b053e88b95f339a5a517d
fet mar <fetmar@gmail.com>
2012-05-21 13:51:38 -0400

    question order partial debug

--- app/_attachments/js/modules/question/Questions.coffee
+++ app/_attachments/js/modules/question/Questions.coffee
@@ -5,4 +5,12 @@ class Questions extends Backbone.Collection
 
   comparator: (subtest) ->
     subtest.get "order"
-  
+
+  # call this after you load the collection you're going to be working with
+  maintainOrder: ->
+    test = (model.get("order") for model in @models).join("")
+    ordered = (i for model,i in @models).join("")
+    if test != ordered
+      for subtest, i in @models
+        subtest.set "order", i
+        subtest.save()
--- app/_attachments/js/modules/question/Questions.js
+++ app/_attachments/js/modules/question/Questions.js
@@ -18,6 +18,40 @@ Questions = (function(_super) {
     return subtest.get("order");
   };
 
+  Questions.prototype.maintainOrder = function() {
+    var i, model, ordered, subtest, test, _len, _ref, _results;
+    test = ((function() {
+      var _i, _len, _ref, _results;
+      _ref = this.models;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        model = _ref[_i];
+        _results.push(model.get("order"));
+      }
+      return _results;
+    }).call(this)).join("");
+    ordered = ((function() {
+      var _len, _ref, _results;
+      _ref = this.models;
+      _results = [];
+      for (i = 0, _len = _ref.length; i < _len; i++) {
+        model = _ref[i];
+        _results.push(i);
+      }
+      return _results;
+    }).call(this)).join("");
+    if (test !== ordered) {
+      _ref = this.models;
+      _results = [];
+      for (i = 0, _len = _ref.length; i < _len; i++) {
+        subtest = _ref[i];
+        subtest.set("order", i);
+        _results.push(subtest.save());
+      }
+      return _results;
+    }
+  };
+
   return Questions;
 
 })(Backbone.Collection);
