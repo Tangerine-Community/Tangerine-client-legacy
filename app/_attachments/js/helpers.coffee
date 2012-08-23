10b3d41bf374cd32b3faa872312bf1587836c155
fet mar <fetmar@gmail.com>
2012-08-23 14:25:01 -0400

    house cleaning

--- app/_attachments/js/helpers.coffee
+++ app/_attachments/js/helpers.coffee
@@ -33,60 +33,9 @@ Backbone.View.prototype.close = ->
     @css "top", (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop() + "px"
     @css "left", (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft() + "px"
 
-  # take a Tangerine "form" and output JSON
-  $.fn.serializeSubtest = ->
-    result = {}
-    $.map $(@).serializeArray(), (element, i) ->
-      if result[element.name]?
-        if $.isArray result[element.name]
-          result[element.name].push element.value
-        else
-          result[element.name] = [result[element.name], element.value]
-      else
-        result[element.name] = element.value
-    result
-
 )(jQuery)
 
 
-class MapReduce
-
-  # Only install this on cloud not on data collection devices
-  @mapFields: (doc, req) ->
-
-    #recursion!
-    concatNodes: (parent,object) ->
-      if object instanceof Array
-        for value, index in object
-          if typeof object != "string"
-            concatNodes(parent+"."+index,value)
-      else
-        typeofobject = typeof object
-
-        if typeofobject == "boolean" or typeofobject == "string" or typeofobject == "number"
-          emitDoc = {
-            studentID: doc.DateTime?["student-id"]
-            fieldname: parent
-          }
-          if typeofobject == "boolean"
-            emitDoc.result = if object then "true" else "false"
-          if typeofobject == "string" or typeofobject == "number"
-            emitDoc.result = object
-          emit doc.assessment, emitDoc
-        else
-          for key,value of object
-            prefix  = (if parent == "" then key else parent + "." + key)
-            concatNodes(prefix,value)
-
-    concatNodes("",doc) unless (doc.type? and doc.type is "replicationLog")
-
-  @reduceFields: (keys, values, rereduce) ->
-    rv = []
-    for key,value of values
-      fieldAndResult = {}
-      fieldAndResult[value.fieldname] = value.result
-      rv.push fieldAndResult
-    return rv
 
 
 String.prototype.safeToSave = ->
--- app/_attachments/js/helpers.js
+++ app/_attachments/js/helpers.js
@@ -1,4 +1,4 @@
-var MapReduce, Utils;
+var Utils;
 
 Backbone.View.prototype.close = function() {
   this.remove();
@@ -24,94 +24,12 @@ Backbone.View.prototype.close = function() {
     this.css("top", $(window).scrollTop() + "px");
     return this.css("left", (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft() + "px");
   };
-  $.fn.middleCenter = function() {
+  return $.fn.middleCenter = function() {
     this.css("position", "absolute");
     this.css("top", (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop() + "px");
     return this.css("left", (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft() + "px");
   };
-  return $.fn.serializeSubtest = function() {
-    var result;
-    result = {};
-    $.map($(this).serializeArray(), function(element, i) {
-      if (result[element.name] != null) {
-        if ($.isArray(result[element.name])) {
-          return result[element.name].push(element.value);
-        } else {
-          return result[element.name] = [result[element.name], element.value];
-        }
-      } else {
-        return result[element.name] = element.value;
-      }
-    });
-    return result;
-  };
 })(jQuery);
-
-MapReduce = (function() {
-
-  function MapReduce() {}
-
-  MapReduce.mapFields = function(doc, req) {
-    ({
-      concatNodes: function(parent, object) {
-        var emitDoc, index, key, prefix, typeofobject, value, _len, _ref, _results, _results2;
-        if (object instanceof Array) {
-          _results = [];
-          for (index = 0, _len = object.length; index < _len; index++) {
-            value = object[index];
-            if (typeof object !== "string") {
-              _results.push(concatNodes(parent + "." + index, value));
-            } else {
-              _results.push(void 0);
-            }
-          }
-          return _results;
-        } else {
-          typeofobject = typeof object;
-          if (typeofobject === "boolean" || typeofobject === "string" || typeofobject === "number") {
-            emitDoc = {
-              studentID: (_ref = doc.DateTime) != null ? _ref["student-id"] : void 0,
-              fieldname: parent
-            };
-            if (typeofobject === "boolean") {
-              emitDoc.result = object ? "true" : "false";
-            }
-            if (typeofobject === "string" || typeofobject === "number") {
-              emitDoc.result = object;
-            }
-            return emit(doc.assessment, emitDoc);
-          } else {
-            _results2 = [];
-            for (key in object) {
-              value = object[key];
-              prefix = (parent === "" ? key : parent + "." + key);
-              _results2.push(concatNodes(prefix, value));
-            }
-            return _results2;
-          }
-        }
-      }
-    });
-    if (!((doc.type != null) && doc.type === "replicationLog")) {
-      return concatNodes("", doc);
-    }
-  };
-
-  MapReduce.reduceFields = function(keys, values, rereduce) {
-    var fieldAndResult, key, rv, value;
-    rv = [];
-    for (key in values) {
-      value = values[key];
-      fieldAndResult = {};
-      fieldAndResult[value.fieldname] = value.result;
-      rv.push(fieldAndResult);
-    }
-    return rv;
-  };
-
-  return MapReduce;
-
-})();
 
 String.prototype.safeToSave = function() {
   return this.replace(/\s|-/g, "_").replace(/[^a-zA-Z0-9_'""]/g, "");
