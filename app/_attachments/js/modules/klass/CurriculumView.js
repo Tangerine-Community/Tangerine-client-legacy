1f99c12f9ff9e3031c20cb37509ae07410ee42f0
fet mar <fetmar@gmail.com>
2012-08-28 03:16:57 -0400

    show curriculum details

--- app/_attachments/js/modules/klass/CurriculumView.js
+++ app/_attachments/js/modules/klass/CurriculumView.js
@@ -10,6 +10,21 @@ CurriculumView = (function(_super) {
     CurriculumView.__super__.constructor.apply(this, arguments);
   }
 
+  CurriculumView.prototype.events = {
+    "click .back": "goBack"
+  };
+
+  CurriculumView.prototype.goBack = function() {
+    return history.back();
+  };
+
+  CurriculumView.prototype.initialize = function(options) {};
+
+  CurriculumView.prototype.render = function() {
+    this.$el.html("    <button class='navigation back'>" + (t('back')) + "</button>    <h1>" + (this.options.curriculum.get('name')) + "</h1>        <table>      <tr><td>" + (t('total subtests')) + "</td><td>" + this.options.subtests.length + "</td></tr>      <tr><td>" + (t('total parts')) + "</td><td>" + this.options.parts + "</td></tr>    </table>");
+    return this.trigger("rendered");
+  };
+
   return CurriculumView;
 
 })(Backbone.View);
