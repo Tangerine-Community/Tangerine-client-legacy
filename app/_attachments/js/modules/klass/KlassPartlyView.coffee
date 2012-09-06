981b6bf6bf7a2858c782e65faa97cfcbb017f3c9
fet mar <fetmar@gmail.com>
2012-09-06 00:46:12 -0400

    student name search. Issue #79

--- app/_attachments/js/modules/klass/KlassPartlyView.coffee
+++ app/_attachments/js/modules/klass/KlassPartlyView.coffee
@@ -5,6 +5,9 @@ class KlassPartlyView extends Backbone.View
     "click .prev_part"                : "prevPart"
     "click .back"                     : "back"
     "click .student_subtest"          : "gotoStudentSubtest"
+    "keyup #current_part"             : "gotoAssessment"
+    "keyup #search_student_name"      : "filterStudents"
+
     #"click .part_subtest_report"      : "partSubtestReport"
     #"click .student"                  : "gotoStudentReport"
 
@@ -15,7 +18,23 @@ class KlassPartlyView extends Backbone.View
 #    id = $(event.target).attr("data-id")
 #    Tangerine.router.navigate "report/#{id}", true
 
-    
+  filterStudents: ->
+    val = @$el.find("#search_student_name").val()
+    @search = val
+    @updateGridPage()
+
+  gotoAssessment: ->
+    val = @$el.find("#current_part").val()
+    if val == "" then return
+    @currentPart = parseInt(val)
+    console.log @$el.find("#current_part").val()
+    @updateGridPage()
+
+  update: ->
+    @render()
+    Tangerine.router.navigate "class/#{@options.klass.id}/#{@currentPart}"
+
+
   back: ->
     Tangerine.router.navigate "class", true
 
@@ -27,16 +46,15 @@ class KlassPartlyView extends Backbone.View
   nextPart: ->
     if @currentPart < @subtestsByPart.length-1
       @currentPart++
-      @render()
-      Tangerine.router.navigate "class/#{@options.klass.id}/#{@currentPart}"
+      @update()
 
   prevPart: -> 
     if @currentPart > 1
       @currentPart-- 
-      @render()
-      Tangerine.router.navigate "class/#{@options.klass.id}/#{@currentPart}"
+      @update()
 
   initialize: (options) ->
+    @search = ""
     @currentPart = options.part || 1
     @subtestsByPart = []
     part = 1
@@ -46,15 +64,14 @@ class KlassPartlyView extends Backbone.View
       part++
     @totalParts = part - 1
 
+  updateGridPage:->
+    @$el.find("#grid_container").html @getGridPage()
 
-  render: ->
-
-    @table = []
-
+  getGridPage: ->
+    table = []
     subtestsThisPart = @subtestsByPart[@currentPart]
-
     for student, i in @options.students.models
-      @table[i] = []
+      table[i] = []
 
       resultsForThisStudent = new KlassResults @options.results.where "studentId" : student.id
 
@@ -62,32 +79,43 @@ class KlassPartlyView extends Backbone.View
         studentResult = resultsForThisStudent.where "subtestId" : subtest.id
         taken = studentResult.length != 0
         
-        @table[i].push
-          "content"   : if taken then "&#x2714;" else "?"
-          "taken"     : taken
-          "studentId" : student.id
-          "studentName" : student.get("name")
-          "subtestId" : subtest.id
+        if ~student.get("name").toLowerCase().indexOf(@search.toLowerCase()) || @search == ""
+          table[i].push
+            "content"   : if taken then "&#x2714;" else "?"
+            "taken"     : taken
+            "studentId" : student.id
+            "studentName" : student.get("name")
+            "subtestId" : subtest.id
 
     # make headers
     gridPage = "<table class='info_box_wide'><tbody><tr><th></th>"
     for subtest in subtestsThisPart
       gridPage += "<th><div class='part_subtest_report' data-id='#{subtest.id}'>#{subtest.get('name')}</div></th>"
     gridPage += "</tr>"
-    for row in @table
-      gridPage += "<tr><td><div class='student' data-studentId='#{row[0].studentId}'>#{row[0].studentName}</div></td>"
-      for cell, column in row
-        takenClass = if cell.taken then " subtest_taken" else ""
-        gridPage += "<td><div class='student_subtest command #{takenClass}' data-taken='#{cell.taken}' data-studentId='#{cell.studentId}' data-subtestId='#{cell.subtestId}'>#{cell.content}</div></td>"
-      gridPage += "</tr>"
+    for row in table
+      if row? && row.length
+        gridPage += "<tr><td><div class='student' data-studentId='#{row[0].studentId}'>#{row[0].studentName}</div></td>"
+        for cell, column in row
+          takenClass = if cell.taken then " subtest_taken" else ""
+          gridPage += "<td><div class='student_subtest command #{takenClass}' data-taken='#{cell.taken}' data-studentId='#{cell.studentId}' data-subtestId='#{cell.subtestId}'>#{cell.content}</div></td>"
+        gridPage += "</tr>"
     gridPage += "</tbody></table>"
 
+    return gridPage
+
+
+  render: ->
+    
+    gridPage = @getGridPage()
+    
     @$el.html "
       <h1>#{t('assessment status')}</h1>
-      #{gridPage}<br>
+      <input id='search_student_name' placeholder='#{t('search student name')}' type='text'>
+
+      <div id='grid_container'>#{gridPage}</div><br>
       <h2>#{t('current assessment')} </h2>
       
-      <button class='prev_part command'>&lt;</button> #{@currentPart} <button class='next_part command'>&gt;</button><br><br>
+      <button class='prev_part command'>&lt;</button> <input type='number' value='#{@currentPart}' id='current_part'> <button class='next_part command'>&gt;</button><br><br>
       <button class='back navigation'>#{t('back')}</button> 
       "
 
--- app/_attachments/js/modules/klass/KlassPartlyView.js
+++ app/_attachments/js/modules/klass/KlassPartlyView.js
@@ -14,7 +14,30 @@ KlassPartlyView = (function(_super) {
     "click .next_part": "nextPart",
     "click .prev_part": "prevPart",
     "click .back": "back",
-    "click .student_subtest": "gotoStudentSubtest"
+    "click .student_subtest": "gotoStudentSubtest",
+    "keyup #current_part": "gotoAssessment",
+    "keyup #search_student_name": "filterStudents"
+  };
+
+  KlassPartlyView.prototype.filterStudents = function() {
+    var val;
+    val = this.$el.find("#search_student_name").val();
+    this.search = val;
+    return this.updateGridPage();
+  };
+
+  KlassPartlyView.prototype.gotoAssessment = function() {
+    var val;
+    val = this.$el.find("#current_part").val();
+    if (val === "") return;
+    this.currentPart = parseInt(val);
+    console.log(this.$el.find("#current_part").val());
+    return this.updateGridPage();
+  };
+
+  KlassPartlyView.prototype.update = function() {
+    this.render();
+    return Tangerine.router.navigate("class/" + this.options.klass.id + "/" + this.currentPart);
   };
 
   KlassPartlyView.prototype.back = function() {
@@ -31,21 +54,20 @@ KlassPartlyView = (function(_super) {
   KlassPartlyView.prototype.nextPart = function() {
     if (this.currentPart < this.subtestsByPart.length - 1) {
       this.currentPart++;
-      this.render();
-      return Tangerine.router.navigate("class/" + this.options.klass.id + "/" + this.currentPart);
+      return this.update();
     }
   };
 
   KlassPartlyView.prototype.prevPart = function() {
     if (this.currentPart > 1) {
       this.currentPart--;
-      this.render();
-      return Tangerine.router.navigate("class/" + this.options.klass.id + "/" + this.currentPart);
+      return this.update();
     }
   };
 
   KlassPartlyView.prototype.initialize = function(options) {
     var byPart, part;
+    this.search = "";
     this.currentPart = options.part || 1;
     this.subtestsByPart = [];
     part = 1;
@@ -61,14 +83,18 @@ KlassPartlyView = (function(_super) {
     return this.totalParts = part - 1;
   };
 
-  KlassPartlyView.prototype.render = function() {
-    var cell, column, gridPage, i, j, resultsForThisStudent, row, student, studentResult, subtest, subtestsThisPart, taken, takenClass, _i, _j, _len, _len2, _len3, _len4, _len5, _ref, _ref2;
-    this.table = [];
+  KlassPartlyView.prototype.updateGridPage = function() {
+    return this.$el.find("#grid_container").html(this.getGridPage());
+  };
+
+  KlassPartlyView.prototype.getGridPage = function() {
+    var cell, column, gridPage, i, j, resultsForThisStudent, row, student, studentResult, subtest, subtestsThisPart, table, taken, takenClass, _i, _j, _len, _len2, _len3, _len4, _len5, _ref;
+    table = [];
     subtestsThisPart = this.subtestsByPart[this.currentPart];
     _ref = this.options.students.models;
     for (i = 0, _len = _ref.length; i < _len; i++) {
       student = _ref[i];
-      this.table[i] = [];
+      table[i] = [];
       resultsForThisStudent = new KlassResults(this.options.results.where({
         "studentId": student.id
       }));
@@ -78,13 +104,15 @@ KlassPartlyView = (function(_super) {
           "subtestId": subtest.id
         });
         taken = studentResult.length !== 0;
-        this.table[i].push({
-          "content": taken ? "&#x2714;" : "?",
-          "taken": taken,
-          "studentId": student.id,
-          "studentName": student.get("name"),
-          "subtestId": subtest.id
-        });
+        if (~student.get("name").toLowerCase().indexOf(this.search.toLowerCase()) || this.search === "") {
+          table[i].push({
+            "content": taken ? "&#x2714;" : "?",
+            "taken": taken,
+            "studentId": student.id,
+            "studentName": student.get("name"),
+            "subtestId": subtest.id
+          });
+        }
       }
     }
     gridPage = "<table class='info_box_wide'><tbody><tr><th></th>";
@@ -93,19 +121,26 @@ KlassPartlyView = (function(_super) {
       gridPage += "<th><div class='part_subtest_report' data-id='" + subtest.id + "'>" + (subtest.get('name')) + "</div></th>";
     }
     gridPage += "</tr>";
-    _ref2 = this.table;
-    for (_j = 0, _len4 = _ref2.length; _j < _len4; _j++) {
-      row = _ref2[_j];
-      gridPage += "<tr><td><div class='student' data-studentId='" + row[0].studentId + "'>" + row[0].studentName + "</div></td>";
-      for (column = 0, _len5 = row.length; column < _len5; column++) {
-        cell = row[column];
-        takenClass = cell.taken ? " subtest_taken" : "";
-        gridPage += "<td><div class='student_subtest command " + takenClass + "' data-taken='" + cell.taken + "' data-studentId='" + cell.studentId + "' data-subtestId='" + cell.subtestId + "'>" + cell.content + "</div></td>";
+    for (_j = 0, _len4 = table.length; _j < _len4; _j++) {
+      row = table[_j];
+      if ((row != null) && row.length) {
+        gridPage += "<tr><td><div class='student' data-studentId='" + row[0].studentId + "'>" + row[0].studentName + "</div></td>";
+        for (column = 0, _len5 = row.length; column < _len5; column++) {
+          cell = row[column];
+          takenClass = cell.taken ? " subtest_taken" : "";
+          gridPage += "<td><div class='student_subtest command " + takenClass + "' data-taken='" + cell.taken + "' data-studentId='" + cell.studentId + "' data-subtestId='" + cell.subtestId + "'>" + cell.content + "</div></td>";
+        }
+        gridPage += "</tr>";
       }
-      gridPage += "</tr>";
     }
     gridPage += "</tbody></table>";
-    this.$el.html("      <h1>" + (t('assessment status')) + "</h1>      " + gridPage + "<br>      <h2>" + (t('current assessment')) + " </h2>            <button class='prev_part command'>&lt;</button> " + this.currentPart + " <button class='next_part command'>&gt;</button><br><br>      <button class='back navigation'>" + (t('back')) + "</button>       ");
+    return gridPage;
+  };
+
+  KlassPartlyView.prototype.render = function() {
+    var gridPage;
+    gridPage = this.getGridPage();
+    this.$el.html("      <h1>" + (t('assessment status')) + "</h1>      <input id='search_student_name' placeholder='" + (t('search student name')) + "' type='text'>      <div id='grid_container'>" + gridPage + "</div><br>      <h2>" + (t('current assessment')) + " </h2>            <button class='prev_part command'>&lt;</button> <input type='number' value='" + this.currentPart + "' id='current_part'> <button class='next_part command'>&gt;</button><br><br>      <button class='back navigation'>" + (t('back')) + "</button>       ");
     return this.trigger("rendered");
   };
 
