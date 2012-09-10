781b58e40f917befa89d471727409bd9b0031508
fet mar <fetmar@gmail.com>
2012-09-10 19:12:12 -0400

    manual importing

--- app/_attachments/js/modules/assessment/AssessmentImportView.coffee
+++ app/_attachments/js/modules/assessment/AssessmentImportView.coffee
@@ -4,58 +4,106 @@ class AssessmentImportView extends Backbone.View
     'click .import' : 'import'
     'click .back'   : 'back'
 
+  initialize: ->
+
+    @serverStatus = "checking..."
+    $.ajax
+      dataType: "jsonp"
+      url: Tangerine.config.address.cloud.host+":"+Tangerine.config.address.port+"/"
+      success: (a, b) =>
+        @serverStatus = "Ok"
+        @updateServerStatus()
+      error: (a, b) =>
+        @serverStatus = "Not available"
+        @updateServerStatus()
+
+  updateServerStatus: ->
+    @$el.find("#server_connection").html @serverStatus
+
   back: ->
     Tangerine.router.navigate "", true
     false
 
-  import: ->
-    
-    # This is supposed to work
-#    $.couch.db("tangerine").compact
-#      complete: (a,b,c) =>
-#        console.log "compact"
-#        console.log [a,b,c]
 
+  import: ->
     dKey = @$el.find("#d_key").val()
     @$el.find(".status").fadeIn(250)
     @$el.find("#progress").html "Looking for #{dKey}"
-    repOps = 
-      'filter' : Tangerine.config.address.designDoc + '/importFilter'
-      'create_target' : true
-      'query_params' :
-        'downloadKey' : dKey
 
-    opts =
-      success: (a, b) =>
-        @$el.find("#progress").html "Import successful <h3>Imported</h3>"
-        # this next step is just a test to see everything is there...
-        # maybe it doesn't need to. Kind of impressive though.
-        Tangerine.$db.view Tangerine.config.address.designDoc + "/byDKey",
-          keys: [dKey]
-          success: (data) =>
-            questions = 0
-            assessments = 0
-            subtests = 0
-            assessmentName = ""
-            for datum in data.rows
-              doc = datum.value
-              subtests++ if doc.collection == 'subtest'
-              questions++ if doc.collection == 'question'  
-              assessmentName = doc.name if doc.collection == 'assessment'
-            @$el.find("#progress").append "
-              <div>#{assessmentName}</div>
-              <div>Subtests - #{subtests}</div>
-              <div>Questions - #{questions}</div>"
+    $.ajax
+      type: "GET"
+      url: "http://tangerine.iriscouch.com/tangerine/_design/tangerine/_view/byDKey?keys=[%22#{dKey}%22]"
+      dataType: "jsonp" 
+      success: (data) =>
+        console.log data
+        for doc in data.rows
+          doc = doc.value
+          Tangerine.$db.openDoc doc._id,
+            success: (oldDoc) =>
+              console.log "work with this:"
+              console.log data
+              doc._rev = oldDoc._rev
+              Tangerine.$db.saveDoc doc,
+                success: =>
+                  console.log "overwrote old doc"
+                error: =>
+                  console.log "could not overwrite old doc"
+                  console.log arguments
+            error  : =>
+              console.log "no doc there: arguments[2]"
+              if arguments[2] == "deleted"
+                console.log "trying to undelete"
+                Tangerine.$db.compact
+                  complete: =>
+                    console.log "compacted database"
+                    Tangerine.$db.saveDoc doc,
+                      success: =>
+                        console.log "saved brand new doc"
+                      error: =>
+                        console.log "could not save brand new doc"
+                        console.log arguments
+                  error: =>
+                    console.log "could not compact"
 
-          error: (a, b ,c) ->
-            @$el.find("#progress").html "<div>Error after data imported</div><div>#{a}</div><div>#{b}"
-
+              else
+                Tangerine.$db.saveDoc doc,
+                  success: =>
+                    console.log "saved brand new doc"
+                  error: =>
+                    console.log "could not save brand new doc"
+                    console.log arguments
+          , 
+            async: false
+            revs_info: true
       error: (a,b) =>
         @$el.find("#progress").html "<div>Import error</div><div>#{a}</div><div>#{b}"
-    
-    $.couch.replicate Tangerine.config.address.cloud.host+":"+Tangerine.config.address.port+"/"+Tangerine.config.address.cloud.dbName, Tangerine.config.address.local.dbName, opts, repOps
+    return
 
-    false
+  showProgress: (status, info) ->
+    if status == "good"
+      @$el.find("#progress").html "Import successful <h3>Imported</h3>"
+      # this next step is just a test to see everything is there...
+      # maybe it doesn't need to. Kind of impressive though.
+      Tangerine.$db.view Tangerine.config.address.designDoc + "/byDKey",
+        keys: [dKey]
+        success: (data) =>
+          questions = 0
+          assessments = 0
+          subtests = 0
+          assessmentName = ""
+          for datum in data.rows
+            doc = datum.value
+            subtests++ if doc.collection == 'subtest'
+            questions++ if doc.collection == 'question'  
+            assessmentName = doc.name if doc.collection == 'assessment'
+          @$el.find("#progress").append "
+            <div>#{assessmentName}</div>
+            <div>Subtests - #{subtests}</div>
+            <div>Questions - #{questions}</div>"
+        error: (a, b ,c) ->
+          @$el.find("#progress").html "<div>Error after data imported</div><div>#{a}</div><div>#{b}"
+    else if status == "bad"
+      @$el.find("#progress").html "<div>Import error</div>#{arguments.join(',')}"
 
   render: ->
     @$el.html "
@@ -65,7 +113,8 @@ class AssessmentImportView extends Backbone.View
     <div class='question'>
       <label for='d_key'>Download key</label>
       <input id='d_key' value=''>
-      <button class='import command'>Import</button>
+      <button class='import command'>Import</button><br>
+      <small>Server connection: <span id='server_connection'>#{@serverStatus}</span></small>
     </div>
 
     <div class='confirmation status'>
--- app/_attachments/js/modules/assessment/AssessmentImportView.js
+++ app/_attachments/js/modules/assessment/AssessmentImportView.js
@@ -15,60 +15,144 @@ AssessmentImportView = (function(_super) {
     'click .back': 'back'
   };
 
+  AssessmentImportView.prototype.initialize = function() {
+    var _this = this;
+    this.serverStatus = "checking...";
+    return $.ajax({
+      dataType: "jsonp",
+      url: Tangerine.config.address.cloud.host + ":" + Tangerine.config.address.port + "/",
+      success: function(a, b) {
+        _this.serverStatus = "Ok";
+        return _this.updateServerStatus();
+      },
+      error: function(a, b) {
+        _this.serverStatus = "Not available";
+        return _this.updateServerStatus();
+      }
+    });
+  };
+
+  AssessmentImportView.prototype.updateServerStatus = function() {
+    return this.$el.find("#server_connection").html(this.serverStatus);
+  };
+
   AssessmentImportView.prototype.back = function() {
     Tangerine.router.navigate("", true);
     return false;
   };
 
   AssessmentImportView.prototype["import"] = function() {
-    var dKey, opts, repOps,
+    var dKey,
       _this = this;
     dKey = this.$el.find("#d_key").val();
     this.$el.find(".status").fadeIn(250);
     this.$el.find("#progress").html("Looking for " + dKey);
-    repOps = {
-      'filter': Tangerine.config.address.designDoc + '/importFilter',
-      'create_target': true,
-      'query_params': {
-        'downloadKey': dKey
-      }
-    };
-    opts = {
-      success: function(a, b) {
-        _this.$el.find("#progress").html("Import successful <h3>Imported</h3>");
-        return Tangerine.$db.view(Tangerine.config.address.designDoc + "/byDKey", {
-          keys: [dKey],
-          success: function(data) {
-            var assessmentName, assessments, datum, doc, questions, subtests, _i, _len, _ref;
-            questions = 0;
-            assessments = 0;
-            subtests = 0;
-            assessmentName = "";
-            _ref = data.rows;
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-              datum = _ref[_i];
-              doc = datum.value;
-              if (doc.collection === 'subtest') subtests++;
-              if (doc.collection === 'question') questions++;
-              if (doc.collection === 'assessment') assessmentName = doc.name;
+    $.ajax({
+      type: "GET",
+      url: "http://tangerine.iriscouch.com/tangerine/_design/tangerine/_view/byDKey?keys=[%22" + dKey + "%22]",
+      dataType: "jsonp",
+      success: function(data) {
+        var doc, _i, _len, _ref, _results;
+        console.log(data);
+        _ref = data.rows;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          doc = _ref[_i];
+          doc = doc.value;
+          _results.push(Tangerine.$db.openDoc(doc._id, {
+            success: function(oldDoc) {
+              console.log("work with this:");
+              console.log(data);
+              doc._rev = oldDoc._rev;
+              return Tangerine.$db.saveDoc(doc, {
+                success: function() {
+                  return console.log("overwrote old doc");
+                },
+                error: function() {
+                  console.log("could not overwrite old doc");
+                  return console.log(arguments);
+                }
+              });
+            },
+            error: function() {
+              console.log("no doc there: arguments[2]");
+              if (arguments[2] === "deleted") {
+                console.log("trying to undelete");
+                return Tangerine.$db.compact({
+                  complete: function() {
+                    console.log("compacted database");
+                    return Tangerine.$db.saveDoc(doc, {
+                      success: function() {
+                        return console.log("saved brand new doc");
+                      },
+                      error: function() {
+                        console.log("could not save brand new doc");
+                        return console.log(arguments);
+                      }
+                    });
+                  },
+                  error: function() {
+                    return console.log("could not compact");
+                  }
+                });
+              } else {
+                return Tangerine.$db.saveDoc(doc, {
+                  success: function() {
+                    return console.log("saved brand new doc");
+                  },
+                  error: function() {
+                    console.log("could not save brand new doc");
+                    return console.log(arguments);
+                  }
+                });
+              }
             }
-            return _this.$el.find("#progress").append("              <div>" + assessmentName + "</div>              <div>Subtests - " + subtests + "</div>              <div>Questions - " + questions + "</div>");
-          },
-          error: function(a, b, c) {
-            return this.$el.find("#progress").html("<div>Error after data imported</div><div>" + a + "</div><div>" + b);
-          }
-        });
+          }, {
+            async: false,
+            revs_info: true
+          }));
+        }
+        return _results;
       },
       error: function(a, b) {
         return _this.$el.find("#progress").html("<div>Import error</div><div>" + a + "</div><div>" + b);
       }
-    };
-    $.couch.replicate(Tangerine.config.address.cloud.host + ":" + Tangerine.config.address.port + "/" + Tangerine.config.address.cloud.dbName, Tangerine.config.address.local.dbName, opts, repOps);
-    return false;
+    });
+  };
+
+  AssessmentImportView.prototype.showProgress = function(status, info) {
+    var _this = this;
+    if (status === "good") {
+      this.$el.find("#progress").html("Import successful <h3>Imported</h3>");
+      return Tangerine.$db.view(Tangerine.config.address.designDoc + "/byDKey", {
+        keys: [dKey],
+        success: function(data) {
+          var assessmentName, assessments, datum, doc, questions, subtests, _i, _len, _ref;
+          questions = 0;
+          assessments = 0;
+          subtests = 0;
+          assessmentName = "";
+          _ref = data.rows;
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            datum = _ref[_i];
+            doc = datum.value;
+            if (doc.collection === 'subtest') subtests++;
+            if (doc.collection === 'question') questions++;
+            if (doc.collection === 'assessment') assessmentName = doc.name;
+          }
+          return _this.$el.find("#progress").append("            <div>" + assessmentName + "</div>            <div>Subtests - " + subtests + "</div>            <div>Questions - " + questions + "</div>");
+        },
+        error: function(a, b, c) {
+          return this.$el.find("#progress").html("<div>Error after data imported</div><div>" + a + "</div><div>" + b);
+        }
+      });
+    } else if (status === "bad") {
+      return this.$el.find("#progress").html("<div>Import error</div>" + (arguments.join(',')));
+    }
   };
 
   AssessmentImportView.prototype.render = function() {
-    this.$el.html("    <button class='back navigation'>Back</button>    <h1>Tangerine Central Import</h1>    <div class='question'>      <label for='d_key'>Download key</label>      <input id='d_key' value=''>      <button class='import command'>Import</button>    </div>    <div class='confirmation status'>      <h2>Status<h2>      <div class='info_box' id='progress'></div>    </div>    ");
+    this.$el.html("    <button class='back navigation'>Back</button>    <h1>Tangerine Central Import</h1>    <div class='question'>      <label for='d_key'>Download key</label>      <input id='d_key' value=''>      <button class='import command'>Import</button><br>      <small>Server connection: <span id='server_connection'>" + this.serverStatus + "</span></small>    </div>    <div class='confirmation status'>      <h2>Status<h2>      <div class='info_box' id='progress'></div>    </div>    ");
     return this.trigger("rendered");
   };
 
