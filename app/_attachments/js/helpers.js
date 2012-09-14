4c3400c0d01ee3667cab2457d14b3117c45bcdf7
fet mar <fetmar@gmail.com>
2012-09-13 21:47:49 -0400

    debugging keys

--- app/_attachments/js/helpers.js
+++ app/_attachments/js/helpers.js
@@ -1,4 +1,4 @@
-var Utils;
+var Utils, i, keyList, km, sks;
 
 Backbone.View.prototype.close = function() {
   this.remove();
@@ -30,6 +30,83 @@ Backbone.View.prototype.close = function() {
     return this.css("left", (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft() + "px");
   };
 })(jQuery);
+
+km = {
+  "0": 48,
+  "1": 49,
+  "2": 50,
+  "3": 51,
+  "4": 52,
+  "5": 53,
+  "6": 54,
+  "7": 55,
+  "8": 56,
+  "9": 57,
+  "a": 65,
+  "b": 66,
+  "c": 67,
+  "d": 68,
+  "e": 69,
+  "f": 70,
+  "g": 71,
+  "h": 72,
+  "i": 73,
+  "j": 74,
+  "k": 75,
+  "l": 76,
+  "m": 77,
+  "n": 78,
+  "o": 79,
+  "p": 80,
+  "q": 81,
+  "r": 82,
+  "s": 83,
+  "t": 84,
+  "u": 85,
+  "v": 86,
+  "w": 87,
+  "x": 88,
+  "y": 89,
+  "z": 90
+};
+
+sks = [
+  {
+    q: (function() {
+      var _results;
+      _results = [];
+      for (i = 0; i <= 6; i++) {
+        _results.push(km["0100set"[i]]);
+      }
+      return _results;
+    })(),
+    i: 0,
+    c: function() {
+      return Tangerine.router.navigate("settings", true);
+    }
+  }
+];
+
+keyList = [];
+
+$(document).keydown(function(e) {
+  var j, sk, _len, _results;
+  keyList.push(e.keyCode);
+  _results = [];
+  for (j = 0, _len = sks.length; j < _len; j++) {
+    sk = sks[j];
+    if (e.keyCode === sks[j].q[sks[j].i++]) {
+      if (sks[j].i === sks[j].q.length) {
+        _results.push(sks[j]['c']());
+      } else {
+        _results.push(void 0);
+      }
+    } else {
+      _results.push(sks[j].i = 0);
+    }
+  }
+  return _results;
+});
 
 String.prototype.safetyDance = function() {
   return this.replace(/\s/g, "_").replace(/[^a-zA-Z0-9_]/g, "");
