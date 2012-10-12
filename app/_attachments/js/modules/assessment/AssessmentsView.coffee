294e2129a74606a6f04567ddfd014525f1d9dae1
fet mar <fetmar@gmail.com>
2012-10-12 11:25:03 -0400

    new assessment collection view

---
+++ app/_attachments/js/modules/assessment/AssessmentsView.coffee
@@ -0,0 +1,32 @@
+class AssessmentsView extends Backbone.View
+
+  tagName   : "ul"
+
+  initialize: (options) ->
+    @allAssessments = options.allAssessments
+    @parent      = options.parent
+    @refresh()
+
+  refresh: (doRender=false) ->
+    @assessments = new Assessments @allAssessments.where( { "group" : @options.group } )
+    @closeViews()
+    @assessmentViews = ( new AssessmentListElementView( { "model" : assessment, "parent" : @ } ) for assessment in @assessments.models )
+    if doRender then @render()
+
+  render: ->
+    if @assessmentViews.length == 0
+      @$el.html "<p class='grey'>No assessments yet. Click <b>new</b> to start making one.</p>"
+    else
+      for view in @assessmentViews
+        view.render()
+        @$el.append view.el
+
+    @trigger "rendered"
+
+  closeViews: ->
+    if @assessmentViews?
+      for view in @assessmentViews
+        view.close()
+
+  onClose: ->
+    @closeViews()
---
+++ app/_attachments/js/modules/assessment/AssessmentsView.js
@@ -0,0 +1,78 @@
+var AssessmentsView,
+  __hasProp = Object.prototype.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+AssessmentsView = (function(_super) {
+
+  __extends(AssessmentsView, _super);
+
+  function AssessmentsView() {
+    AssessmentsView.__super__.constructor.apply(this, arguments);
+  }
+
+  AssessmentsView.prototype.tagName = "ul";
+
+  AssessmentsView.prototype.initialize = function(options) {
+    this.allAssessments = options.allAssessments;
+    this.parent = options.parent;
+    return this.refresh();
+  };
+
+  AssessmentsView.prototype.refresh = function(doRender) {
+    var assessment;
+    if (doRender == null) doRender = false;
+    this.assessments = new Assessments(this.allAssessments.where({
+      "group": this.options.group
+    }));
+    this.closeViews();
+    this.assessmentViews = (function() {
+      var _i, _len, _ref, _results;
+      _ref = this.assessments.models;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        assessment = _ref[_i];
+        _results.push(new AssessmentListElementView({
+          "model": assessment,
+          "parent": this
+        }));
+      }
+      return _results;
+    }).call(this);
+    if (doRender) return this.render();
+  };
+
+  AssessmentsView.prototype.render = function() {
+    var view, _i, _len, _ref;
+    if (this.assessmentViews.length === 0) {
+      this.$el.html("<p class='grey'>No assessments yet. Click <b>new</b> to start making one.</p>");
+    } else {
+      _ref = this.assessmentViews;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        view = _ref[_i];
+        view.render();
+        this.$el.append(view.el);
+      }
+    }
+    return this.trigger("rendered");
+  };
+
+  AssessmentsView.prototype.closeViews = function() {
+    var view, _i, _len, _ref, _results;
+    if (this.assessmentViews != null) {
+      _ref = this.assessmentViews;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        view = _ref[_i];
+        _results.push(view.close());
+      }
+      return _results;
+    }
+  };
+
+  AssessmentsView.prototype.onClose = function() {
+    return this.closeViews();
+  };
+
+  return AssessmentsView;
+
+})(Backbone.View);
