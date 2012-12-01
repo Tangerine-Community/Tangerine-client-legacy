6bb343c7daa8e74a0d53562602e370a3fba99fb3
fet mar <fetmar@gmail.com>
2012-12-01 10:26:53 -0500

    added back button, css classname, update initialize with cleaner Collection methods, updated table css

--- app/_attachments/js/modules/report/MasteryCheckView.coffee
+++ app/_attachments/js/modules/report/MasteryCheckView.coffee
@@ -1,46 +1,42 @@
 class MasteryCheckView extends Backbone.View
 
+  className : "MasteryCheckView"
+
+  events :
+    "click .back" : "goBack"
+    
+  goBack: -> history.back()
+
   initialize: (options) ->
+
     @results = options.results
     @student = options.student
-    @klass = options.klass
+    @klass   = options.klass
 
-    @resultsByPart = []
-    bucketList = {}
-    for result in @results.models
-      bucketList[result.get("resultBucket")] = true
-      part = result.get("part")
-      @resultsByPart[part] = [] if not @resultsByPart[part]?
-      @resultsByPart[part].push result
-      @resultsByPart[part].sort (a, b) -> return a.attributes.reportType < b.attributes.reportType
-    bucketList = _.keys(bucketList)
-
-    # get current part since start
-    milisecondsPerPart = 604800000
-    @currentPart = Math.round(((new Date()).getTime() - @klass.get("startDate")) / milisecondsPerPart)
-
+    @resultsByPart = @results.indexBy "part"
+    @lastPart = Math.max.apply @, @results.pluck("part")
 
   render: ->
     html = "
       <h1>Mastery check report</h1>
-      <h2>#{@student.get("name")}</h2>
-      <table class='mastery_check'>
+      <h2>Student #{@student.get("name")}</h2>
+      <table>
     "
-    for part in [1..@currentPart]
+    for part in [1..@lastPart]
       if @resultsByPart[part] == undefined then continue
-      html += "<tr>"
-      html += "<td>Week</td><td>#{part}</td>"
+      html += "
+        <tr><th>Assessment #{part}</th></tr>
+        <tr>"
 
-      for bucketResult in @resultsByPart[part]
-        bucketName = bucketResult.attributes.resultBucket.titleize()
-        correct = 0
-        for item in bucketResult.attributes.subtestData.items
-          correct++ if item.itemResult == "correct" 
-        total   = bucketResult.attributes.subtestData.items.length
-        
-        html += "<td>#{bucketName} correct</td><td>#{correct}/#{total}</td>"
+      for result in @resultsByPart[part]
+        html += "
+          <td>#{result.get("itemType").titleize()} correct</td>
+          <td>#{result.get("correct")}/#{result.get("total")}</td>"
       
-    html += "</table>"
+    html += "
+    </table>
+    <button class='navigation back'>#{t('back')}</button>
+    "
     @$el.html html
 
     @trigger "rendered"
--- app/_attachments/js/modules/report/MasteryCheckView.js
+++ app/_attachments/js/modules/report/MasteryCheckView.js
@@ -10,51 +10,37 @@ MasteryCheckView = (function(_super) {
     MasteryCheckView.__super__.constructor.apply(this, arguments);
   }
 
+  MasteryCheckView.prototype.className = "MasteryCheckView";
+
+  MasteryCheckView.prototype.events = {
+    "click .back": "goBack"
+  };
+
+  MasteryCheckView.prototype.goBack = function() {
+    return history.back();
+  };
+
   MasteryCheckView.prototype.initialize = function(options) {
-    var bucketList, milisecondsPerPart, part, result, _i, _len, _ref;
     this.results = options.results;
     this.student = options.student;
     this.klass = options.klass;
-    this.resultsByPart = [];
-    bucketList = {};
-    _ref = this.results.models;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      result = _ref[_i];
-      bucketList[result.get("resultBucket")] = true;
-      part = result.get("part");
-      if (!(this.resultsByPart[part] != null)) this.resultsByPart[part] = [];
-      this.resultsByPart[part].push(result);
-      this.resultsByPart[part].sort(function(a, b) {
-        return a.attributes.reportType < b.attributes.reportType;
-      });
-    }
-    bucketList = _.keys(bucketList);
-    milisecondsPerPart = 604800000;
-    return this.currentPart = Math.round(((new Date()).getTime() - this.klass.get("startDate")) / milisecondsPerPart);
+    this.resultsByPart = this.results.indexBy("part");
+    return this.lastPart = Math.max.apply(this, this.results.pluck("part"));
   };
 
   MasteryCheckView.prototype.render = function() {
-    var bucketName, bucketResult, correct, html, item, part, total, _i, _j, _len, _len2, _ref, _ref2, _ref3;
-    html = "      <h1>Mastery check report</h1>      <h2>" + (this.student.get("name")) + "</h2>      <table class='mastery_check'>    ";
-    for (part = 1, _ref = this.currentPart; 1 <= _ref ? part <= _ref : part >= _ref; 1 <= _ref ? part++ : part--) {
+    var html, part, result, _i, _len, _ref, _ref2;
+    html = "      <h1>Mastery check report</h1>      <h2>Student " + (this.student.get("name")) + "</h2>      <table>    ";
+    for (part = 1, _ref = this.lastPart; 1 <= _ref ? part <= _ref : part >= _ref; 1 <= _ref ? part++ : part--) {
       if (this.resultsByPart[part] === void 0) continue;
-      html += "<tr>";
-      html += "<td>Week</td><td>" + part + "</td>";
+      html += "        <tr><th>Assessment " + part + "</th></tr>        <tr>";
       _ref2 = this.resultsByPart[part];
       for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
-        bucketResult = _ref2[_i];
-        bucketName = bucketResult.attributes.resultBucket.titleize();
-        correct = 0;
-        _ref3 = bucketResult.attributes.subtestData.items;
-        for (_j = 0, _len2 = _ref3.length; _j < _len2; _j++) {
-          item = _ref3[_j];
-          if (item.itemResult === "correct") correct++;
-        }
-        total = bucketResult.attributes.subtestData.items.length;
-        html += "<td>" + bucketName + " correct</td><td>" + correct + "/" + total + "</td>";
+        result = _ref2[_i];
+        html += "          <td>" + (result.get("itemType").titleize()) + " correct</td>          <td>" + (result.get("correct")) + "/" + (result.get("total")) + "</td>";
       }
     }
-    html += "</table>";
+    html += "    </table>    <button class='navigation back'>" + (t('back')) + "</button>    ";
     this.$el.html(html);
     return this.trigger("rendered");
   };
