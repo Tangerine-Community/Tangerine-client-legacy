90629e051608eaa126af273583207b2ea2d81a08
fet mar <fetmar@gmail.com>
2012-12-27 13:53:48 -0500

    tree with bugs

--- tree/tree.rb
+++ tree/tree.rb
@@ -15,13 +15,32 @@ require 'json'
 require 'sqlite3'
 require 'logger'
 
+class Storage
+  
+  def initialize(table_name)
+    @@db = SQLite3::Database.new( DB_NAME )
+    @db_name = table_name
+    @@db.execute( %~ CREATE TABLE IF NOT EXISTS #{db_name} (key varchar(100) PRIMARY KEY, value varchar(1000), modified timestamp(20)) ~ )
+  end
+
+  def self.get(key)
+    @@db.get_first_row( %~ SELECT * FROM #{db_name} WHERE key='#{key}' ~ )
+  end
+
+  def self.set(key, val)
+    result = @@db.execute( %~ REPLACE INTO #{db_name} (key, value, modified) VALUES ('%s', '%s', %d) ~ % [key, val, Time.now.to_i] )
+  end
+  
+  DB_NAME = 'com.rti.tangerine'
+  
+end
 #
 #
 #
 
 $character_set = "abcdeghikmnoprstuwxyz".split("") # optimized for mobiles and human error
 $logger = Logger.new "tree.log"
-$couchdb_path = File.join( `locate tangerine.couch`.split("/")[0..-2] )
+$couch_db_path = File.join( `locate *.couch`.split("\n")[0].split("/")[0..-2] )
 
 #
 # handle a request to make an APK
@@ -36,41 +55,41 @@ post "/production/:group" do
   auth_errors << "a password" if params[:pass] == nil
   auth_errors << "a group"    if params[:group].length == 0
 
-  halt 403, { :error => "Please provide #{andify(auth_requirements)}."} if auth_errors.length > 0
+  #halt 403, { :error => "Please provide #{andify(auth_errors)}."} if auth_errors.length > 0
 
   #
   # Authenticate user for the group
   #
 
-  auth_response = RestClient.get("https://www.tangerinecentra.org/robbert.php?action=am_admin&group=#{:group}&user=#{:user}")
+  auth_response = RestClient.post "http://localhost/robbert.php?", :action=> "am_admin", :group => params[:group], :user => params[:user]
 
-  halt_error(403, "Sorry, you have to be an admin within the group to make an APK.") if JSON.parse(auth_response.body).answer == "no"
+  halt_error(403, "Sorry, you have to be an admin within the group to make an APK.") if JSON.parse(auth_response.body)[:message] == "no"
 
   #
   # Make APK, place it in token-directory for download
   #
 
   created = Time.now
-  storage = new Storage("tree")
+  #storage = new Storage("tree")
   token = get_token()
 
   #
   # check to see if we have the "newest version"
   #
-  apk_list_response = RestClient.get("https://api.github.com/repos/tangerine-community/tangerine/downloads")
-  apk_list = JSON.parse(apk_list_response.body)
+  #apk_list_response = RestClient.get("https://api.github.com/repos/tangerine-community/tangerine/downloads", :Origin => "http://localhost")
+  #apk_list = JSON.parse(apk_list_response.body)
+  #
+  #for list in apk_list
+  #  if apk["name"].include? "blank"
+  #    stable_apk = apk
+  #    break
+  #  end
+  #end
+  
+  #local_apk_date = Date.strptime(storage.get("apk")['created_at'], "%Y-%m-%d")
+  #git_apk_date   = Date.strptime(apk_list[0]["created_at"], "%Y-%m-%d")
 
-  for list in apk_list
-    if apk["name"].include? "blank"
-      stable_apk = apk
-      break
-    end
-  end
-
-  local_apk_date = Date.strptime(storage.get("apk")['created_at'], "%Y-%m-%d")
-  git_apk_date   = Date.strptime(apk_list[0]["created_at"], "%Y-%m-%d")
-
-  if local_apk_date >= git_apk_date
+  if false#local_apk_date >= git_apk_date
     apk_response = RestClient.get( stable_apk['url'] )
     begin
 
@@ -85,9 +104,9 @@ post "/production/:group" do
   end
 
   # replicate group to new local here
-  replicate_response = RestClient.post("http://admin:password@tangerine.iriscouch.com:5984/_replicate", {
+  replicate_response = RestClient.post("http://admin:password@localhost:5984/_replicate", {
     :source => "group-#{params[:group]}", 
-    :target => "http://tangerine.xen.prgmr.com:5984/group-#{params[:group]}",
+    :target => "copied-group-#{params[:group]}",
     :create_target => true
   }.to_json, :content_type => :json )
 
@@ -95,18 +114,23 @@ post "/production/:group" do
 
   begin
     # clear it out if it's there
-    `rm -rf tangerine/`
+    blank_dir = File.join( Dir.pwd, "blank" )
+    `rm -rf #{blank_dir}`
+
+    tangerine_apk = File.join( Dir.pwd, "tangerine.zip" )
 
     #
-    `unzip tangerine.apk -d blank/`
-    group_db = File.join( $couch_db_path, "group-#{params[:group]}.couch" )
+    `unzip #{tangerine_apk} -d blank`
+    db_file = "copied-group-#{params[:group]}.couch"
+    group_db = File.join( $couch_db_path, db_file )
 
     # standardize all groups DBs here as tangerine.couch
-    target_file = File.join("blank", "assets", "tangerine.couch")
-    `cp #{group_db} #{target_file}`
+    target_dir = File.join( Dir.pwd, "blank", "assets" )
+    `cp #{group_db} #{target_dir}`
+    `mv #{db_file} tangerine.couch`
 
   rescue Exception => e
-    log.error "Could not copy #{param[:group]}'s database into assets. #{e}"
+    $logger.error "Could not copy #{params[:group]}'s database into assets. #{e}"
     halt_error 500, "Failed to prepare database."
   end
 
@@ -114,13 +138,16 @@ post "/production/:group" do
   # zip APK and place it in token download directory
   begin
     mkdir(token)
+    puts "token"
+    puts token
     new_zip = File.join( "..", "apks", token, "tangerine.apk" )
+    puts  new_zip
     `cd blank`
     `unzip -r #{new_zip} *`
     `cd ..`
     `rm -rf blank/`
   rescue Exception => e
-    log.error "Could not copy #{param[:group]}'s database into assets. #{e}"
+    log.error "Could not copy #{params[:group]}'s database into assets. #{e}"
     halt_error 500, "Failed to prepare database."
   end
   
@@ -154,13 +181,13 @@ end
 #
 
 def andify( nouns )
-  last = words.pop()
-  return words.join(", ") + ", and " + last
+  #last = nouns.pop()
+  return nouns#nouns.join(", ") + ", and " + last
 end
 
 def orify( nouns )
-  last = words.pop()
-  return words.join(", ") + ", and " + last
+  #last = nouns.pop()
+  return nouns#nouns.join(", ") + ", or " + last
 end
 
 def rand_char()
@@ -181,24 +208,4 @@ end
 
 def halt_error(code, message)
   halt code, { :error => message }.to_json
-end
-
-class Storage
-  
-  def initialize(table_name)
-    @@db = SQLite3::Database.new( DB_NAME )
-    @db_name = table_name
-    @@db.execute( %~ CREATE TABLE IF NOT EXISTS #{db_name} (key varchar(100) PRIMARY KEY, value varchar(1000), modified timestamp(20)) ~ )
-  end
-
-  def self.get(key)
-    @@db.get_first_row( %~ SELECT * FROM #{db_name} WHERE key='#{key}' ~ )
-  end
-
-  def self.set(key, val)
-    result = @@db.execute( %~ REPLACE INTO #{db_name} (key, value, modified) VALUES ('%s', '%s', %d) ~ % [key, val, Time.now.to_i] )
-  end
-  
-  DB_NAME = 'com.rti.tangerine'
-  
 end
