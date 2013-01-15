f44aa0eeb0d0242a37ebe8dd95807b7e281bd7ac
fet mar <fetmar@gmail.com>
2013-01-15 01:27:41 -0500

    only show new results by default

--- app/_attachments/js/modules/klass/KlassResults.js
+++ app/_attachments/js/modules/klass/KlassResults.js
@@ -14,6 +14,29 @@ KlassResults = (function(_super) {
 
   KlassResults.prototype.model = KlassResult;
 
+  KlassResults.prototype.initialize = function(options) {
+    var _this = this;
+    if (!((options.showOld != null) && options.showOld === true)) {
+      return this.on("all", function(event) {
+        var result, resultId, toRemove, _i, _j, _len, _len2, _ref, _results;
+        toRemove = [];
+        _ref = _this.models;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          result = _ref[_i];
+          if (result.has("old")) toRemove.push(result.id);
+        }
+        _results = [];
+        for (_j = 0, _len2 = toRemove.length; _j < _len2; _j++) {
+          resultId = toRemove[_j];
+          _results.push(_this.remove(resultId, {
+            silent: true
+          }));
+        }
+        return _results;
+      });
+    }
+  };
+
   return KlassResults;
 
 })(Backbone.Collection);
