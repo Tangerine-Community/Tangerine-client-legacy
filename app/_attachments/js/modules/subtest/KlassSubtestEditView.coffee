ec7960b2fcd0502f3dff9d012968b4f7b9af74b7
fet mar <fetmar@gmail.com>
2013-01-16 19:42:32 -0500

    slimmed down class subtest editor

--- app/_attachments/js/modules/subtest/KlassSubtestEditView.coffee
+++ app/_attachments/js/modules/subtest/KlassSubtestEditView.coffee
@@ -3,8 +3,12 @@ class KlassSubtestEditView extends Backbone.View
   className: "subtest_edit"
 
   events :
-    'click .back_button'         : 'goBack'
-    'click .save_subtest'        : 'save'
+    'click .back_button'  : 'goBack'
+    'click .save_subtest' : 'save'
+    'blur #subtest_items' : 'cleanWhitespace'
+
+  cleanWhitespace: ->
+    @$el.find("#subtest_items").val( @$el.find("#subtest_items").val().replace(/\s+/g, ' ') )
 
   onClose: ->
     @prototypeEditor.close?()
@@ -25,37 +29,44 @@ class KlassSubtestEditView extends Backbone.View
 
   save: (event) ->
 
-    isEditSave = event?.options?.editSave == true
-
-    prototype = @model.get("prototype")
-    @model.set
+    @model.save
       name           : @$el.find("#name").val()
       part           : parseInt( @$el.find("#part").val() )
       reportType     : @$el.find("#report_type").val()
       itemType       : @$el.find("#item_type").val()
+      scoreTarget    : @$el.find("#score_target").val()
+      scoreSpread    : @$el.find("#score_spread").val()
+      order          : @$el.find("#order").val()
 
-    @prototypeEditor.save?(
-      "options" :
-        "isEditSave" : isEditSave
-    )
-    
-    if @prototypeEditor.isValid? && @prototypeEditor.isValid() == false && not isEditSave
-      Utils.midAlert "There are errors on this page"
-      @prototypeEditor.showErrors?()
-    else
-      if @model.save()
+      endOfLine : @$el.find("#end_of_line input:checked").val() == "true"
+      randomize : @$el.find("#randomize input:checked").val() == "true"
+      timer     : parseInt( @$el.find("#subtest_timer").val() )
+      items     : _.compact( @$el.find("#subtest_items").val().split(" ") ) # mild sanitization, happens at read too
+      columns   : parseInt( @$el.find("#subtest_columns").val() )
+    ,
+      success: =>
         Utils.midAlert "Subtest Saved"
-        setTimeout @goBack, 1000 unless isEditSave
-      else
-        console.log "save error"
+      error: =>
         Utils.midAlert "Save error"
 
   render: ->
+
     curriculumName = @curriculum.escape "name"
     name           = @model.escape "name"
     part           = @model.getNumber "part"
     reportType     = @model.escape "reportType"
     itemType       = @model.escape "itemType"
+
+    scoreTarget    = @model.getNumber "scoreTarget"
+    scoreSpread    = @model.getNumber "scoreSpread"
+    order          = @model.getNumber "order"
+
+    endOfLine    = if @model.has("endOfLine") then @model.get("endOfLine") else true
+    randomize    = if @model.has("randomize") then @model.get("randomize") else false
+
+    items        = @model.get("items").join " "
+    timer        = @model.get("timer")        || 0
+    columns      = @model.get("columns")      || 0
 
 
     @$el.html "
@@ -67,6 +78,7 @@ class KlassSubtestEditView extends Backbone.View
           <td>#{curriculumName}</td>
         </tr>
       </table>
+
       <button class='save_subtest command'>Done</button>
 
       <div class='label_value'>
@@ -89,13 +101,54 @@ class KlassSubtestEditView extends Backbone.View
         <input type='number' id='part' value='#{part}'>
       </div>
 
-      <div id='prototype_attributes'></div>
+      <div class='label_value'>
+        <label for='score_target'>Target score</label><br>
+        <input type='number' id='score_target' value='#{scoreTarget}'>
+      </div>
+
+      <div class='label_value'>
+        <label for='score_spread'>Score spread</label><br>
+        <input type='number' id='score_spread' value='#{scoreSpread}'>
+      </div>
+
+      <div class='label_value'>
+        <label for='order'>Order</label><br>
+        <input type='number' id='order' value='#{order}'>
+      </div>
+
+      <div class='label_value'>
+        <label for='subtest_items' title='These items are space delimited. Pasting text from other applications may insert tabs and new lines. Whitespace will be automatically corrected.'>Grid Items</label>
+        <textarea id='subtest_items'>#{items}</textarea>
+      </div>
+
+      <label>Randomize items</label><br>
+      <div class='menu_box'>
+        <div id='randomize' class='buttonset'>
+          <label for='randomize_true'>Yes</label><input name='randomize' type='radio' value='true' id='randomize_true' #{'checked' if randomize}>
+          <label for='randomize_false'>No</label><input name='randomize' type='radio' value='false' id='randomize_false' #{'checked' if not randomize}>
+        </div>
+      </div><br>
+
+      <label>Mark entire line button</label><br>
+      <div class='menu_box'>
+        <div id='end_of_line' class='buttonset'>
+          <label for='end_of_line_true'>Yes</label><input name='end_of_line' type='radio' value='true' id='end_of_line_true' #{'checked' if endOfLine}>
+          <label for='end_of_line_false'>No</label><input name='end_of_line' type='radio' value='false' id='end_of_line_false' #{'checked' if not endOfLine}>
+        </div>
+      </div>
+
+      <div class='label_value'>
+        <label for='subtest_columns' title='Number of columns in which to display the grid items.'>Columns</label><br>
+        <input id='subtest_columns' value='#{columns}' type='number'>
+      </div>
+
+      <div class='label_value'>
+        <label for='subtest_timer' title='Seconds to give the child to complete the test. Setting this value to 0 will make the test untimed.'>Timer</label><br>
+        <input id='subtest_timer' value='#{timer}' type='number'>
+      </div>
 
       <button class='save_subtest command'>Done</button>
       "
-
-    @prototypeEditor.setElement @$el.find('#prototype_attributes')
-    @prototypeEditor.render?()
 
     @trigger "rendered"
 
--- app/_attachments/js/modules/subtest/KlassSubtestEditView.js
+++ app/_attachments/js/modules/subtest/KlassSubtestEditView.js
@@ -16,7 +16,12 @@ KlassSubtestEditView = (function(_super) {
 
   KlassSubtestEditView.prototype.events = {
     'click .back_button': 'goBack',
-    'click .save_subtest': 'save'
+    'click .save_subtest': 'save',
+    'blur #subtest_items': 'cleanWhitespace'
+  };
+
+  KlassSubtestEditView.prototype.cleanWhitespace = function() {
+    return this.$el.find("#subtest_items").val(this.$el.find("#subtest_items").val().replace(/\s+/g, ' '));
   };
 
   KlassSubtestEditView.prototype.onClose = function() {
@@ -48,48 +53,46 @@ KlassSubtestEditView = (function(_super) {
   };
 
   KlassSubtestEditView.prototype.save = function(event) {
-    var isEditSave, prototype, _base, _base2, _ref;
-    isEditSave = (event != null ? (_ref = event.options) != null ? _ref.editSave : void 0 : void 0) === true;
-    prototype = this.model.get("prototype");
-    this.model.set({
+    var _this = this;
+    return this.model.save({
       name: this.$el.find("#name").val(),
       part: parseInt(this.$el.find("#part").val()),
       reportType: this.$el.find("#report_type").val(),
-      itemType: this.$el.find("#item_type").val()
-    });
-    if (typeof (_base = this.prototypeEditor).save === "function") {
-      _base.save({
-        "options": {
-          "isEditSave": isEditSave
-        }
-      });
-    }
-    if ((this.prototypeEditor.isValid != null) && this.prototypeEditor.isValid() === false && !isEditSave) {
-      Utils.midAlert("There are errors on this page");
-      return typeof (_base2 = this.prototypeEditor).showErrors === "function" ? _base2.showErrors() : void 0;
-    } else {
-      if (this.model.save()) {
-        Utils.midAlert("Subtest Saved");
-        if (!isEditSave) return setTimeout(this.goBack, 1000);
-      } else {
-        console.log("save error");
+      itemType: this.$el.find("#item_type").val(),
+      scoreTarget: this.$el.find("#score_target").val(),
+      scoreSpread: this.$el.find("#score_spread").val(),
+      order: this.$el.find("#order").val(),
+      endOfLine: this.$el.find("#end_of_line input:checked").val() === "true",
+      randomize: this.$el.find("#randomize input:checked").val() === "true",
+      timer: parseInt(this.$el.find("#subtest_timer").val()),
+      items: _.compact(this.$el.find("#subtest_items").val().split(" ")),
+      columns: parseInt(this.$el.find("#subtest_columns").val())
+    }, {
+      success: function() {
+        return Utils.midAlert("Subtest Saved");
+      },
+      error: function() {
         return Utils.midAlert("Save error");
       }
-    }
+    });
   };
 
   KlassSubtestEditView.prototype.render = function() {
-    var curriculumName, itemType, name, part, reportType, _base;
+    var columns, curriculumName, endOfLine, itemType, items, name, order, part, randomize, reportType, scoreSpread, scoreTarget, timer;
     curriculumName = this.curriculum.escape("name");
     name = this.model.escape("name");
     part = this.model.getNumber("part");
     reportType = this.model.escape("reportType");
     itemType = this.model.escape("itemType");
-    this.$el.html("      <button class='back_button navigation'>Back</button><br>      <h1>Subtest Editor</h1>      <table class='basic_info'>        <tr>          <th>Curriculum</th>          <td>" + curriculumName + "</td>        </tr>      </table>      <button class='save_subtest command'>Done</button>      <div class='label_value'>        <label for='name'>Name</label>        <input id='name' value='" + name + "'>      </div>      <div class='label_value'>        <label for='report_type'>Report Type</label>        <input id='report_type' value='" + reportType + "'>      </div>      <div class='label_value'>        <label for='item_type'>Item Type</label>        <input id='item_type' value='" + itemType + "'>      </div>      <div class='label_value'>        <label for='part'>Assessment Number</label><br>        <input type='number' id='part' value='" + part + "'>      </div>      <div id='prototype_attributes'></div>      <button class='save_subtest command'>Done</button>      ");
-    this.prototypeEditor.setElement(this.$el.find('#prototype_attributes'));
-    if (typeof (_base = this.prototypeEditor).render === "function") {
-      _base.render();
-    }
+    scoreTarget = this.model.getNumber("scoreTarget");
+    scoreSpread = this.model.getNumber("scoreSpread");
+    order = this.model.getNumber("order");
+    endOfLine = this.model.has("endOfLine") ? this.model.get("endOfLine") : true;
+    randomize = this.model.has("randomize") ? this.model.get("randomize") : false;
+    items = this.model.get("items").join(" ");
+    timer = this.model.get("timer") || 0;
+    columns = this.model.get("columns") || 0;
+    this.$el.html("      <button class='back_button navigation'>Back</button><br>      <h1>Subtest Editor</h1>      <table class='basic_info'>        <tr>          <th>Curriculum</th>          <td>" + curriculumName + "</td>        </tr>      </table>      <button class='save_subtest command'>Done</button>      <div class='label_value'>        <label for='name'>Name</label>        <input id='name' value='" + name + "'>      </div>      <div class='label_value'>        <label for='report_type'>Report Type</label>        <input id='report_type' value='" + reportType + "'>      </div>      <div class='label_value'>        <label for='item_type'>Item Type</label>        <input id='item_type' value='" + itemType + "'>      </div>      <div class='label_value'>        <label for='part'>Assessment Number</label><br>        <input type='number' id='part' value='" + part + "'>      </div>      <div class='label_value'>        <label for='score_target'>Target score</label><br>        <input type='number' id='score_target' value='" + scoreTarget + "'>      </div>      <div class='label_value'>        <label for='score_spread'>Score spread</label><br>        <input type='number' id='score_spread' value='" + scoreSpread + "'>      </div>      <div class='label_value'>        <label for='order'>Order</label><br>        <input type='number' id='order' value='" + order + "'>      </div>      <div class='label_value'>        <label for='subtest_items' title='These items are space delimited. Pasting text from other applications may insert tabs and new lines. Whitespace will be automatically corrected.'>Grid Items</label>        <textarea id='subtest_items'>" + items + "</textarea>      </div>      <label>Randomize items</label><br>      <div class='menu_box'>        <div id='randomize' class='buttonset'>          <label for='randomize_true'>Yes</label><input name='randomize' type='radio' value='true' id='randomize_true' " + (randomize ? 'checked' : void 0) + ">          <label for='randomize_false'>No</label><input name='randomize' type='radio' value='false' id='randomize_false' " + (!randomize ? 'checked' : void 0) + ">        </div>      </div><br>      <label>Mark entire line button</label><br>      <div class='menu_box'>        <div id='end_of_line' class='buttonset'>          <label for='end_of_line_true'>Yes</label><input name='end_of_line' type='radio' value='true' id='end_of_line_true' " + (endOfLine ? 'checked' : void 0) + ">          <label for='end_of_line_false'>No</label><input name='end_of_line' type='radio' value='false' id='end_of_line_false' " + (!endOfLine ? 'checked' : void 0) + ">        </div>      </div>      <div class='label_value'>        <label for='subtest_columns' title='Number of columns in which to display the grid items.'>Columns</label><br>        <input id='subtest_columns' value='" + columns + "' type='number'>      </div>      <div class='label_value'>        <label for='subtest_timer' title='Seconds to give the child to complete the test. Setting this value to 0 will make the test untimed.'>Timer</label><br>        <input id='subtest_timer' value='" + timer + "' type='number'>      </div>      <button class='save_subtest command'>Done</button>      ");
     return this.trigger("rendered");
   };
 
