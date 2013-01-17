8f13323e40075130120e1256fb06f7d4fb6880a4
fet mar <fetmar@gmail.com>
2013-01-16 19:43:03 -0500

    view for group imports

--- app/views/assessmentsNotArchived/map.js
+++ app/views/assessmentsNotArchived/map.js
@@ -1,6 +1,6 @@
 function(doc)
 { 
-	if (doc.collection==="assessment" && (doc.archived === "false" || doc.archived === false))
+	if ((doc.collection==="curriculum" || doc.collection==="assessment") && (doc.archived === "false" || doc.archived === false))
 	{
 		emit(doc.id, null);
 	}
