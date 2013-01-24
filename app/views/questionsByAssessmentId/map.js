3439dc46b36be1d91ab34d517f56e0019151cd84
fet mar <fetmar@gmail.com>
2013-01-24 16:46:48 -0500

    questions by assessment or curriculum id

--- app/views/questionsByAssessmentId/map.js
+++ app/views/questionsByAssessmentId/map.js
@@ -1,5 +1,11 @@
 function(doc) {
-  if (doc.collection == 'question') {
-    emit(doc.assessmentId, doc);
-  }
+	if (doc.collection == 'question') {
+		if (doc.assessmentId)
+		{
+			emit(doc.assessmentId, doc);
+		} else if (doc.curriculumId)
+		{
+			emit(doc.curriculumId, doc);
+		}
+	}
 }
