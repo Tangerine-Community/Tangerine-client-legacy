131069155dfa68661649f0ed89c692e9bcadab75
fet mar <fetmar@gmail.com>
2013-06-26 19:53:38 -0400

    use relative paths because some people don't like . in their $PATH

--- app/_attachments/js/init.sh
+++ app/_attachments/js/init.sh
@@ -4,7 +4,7 @@ else
   rm min/*
 fi
 rm lib.js app.js
-uglify.rb `find ./modules -name "*.js"`
-uglify.rb `ls *.js`
-uglify.rb lib
-uglify.rb app
+./uglify.rb `find ./modules -name "*.js"`
+./uglify.rb `ls *.js`
+./uglify.rb lib
+./uglify.rb app
